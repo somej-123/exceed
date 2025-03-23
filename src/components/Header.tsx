7f76e4d98e4ed9a819df2caf61d6e4450792d8a8
woojungho <somej@naver.com>
2025-03-24 02:41:05 +0900

    레이아웃 구성 및 메인 홈 페이지 수정 작업 진행 중중

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,39 @@
+import HeaderCss from '../styles/Header.module.css';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) => {
+    return location.pathname === path ? HeaderCss.activeLink : '';
+  };
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Exceed</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className={`${HeaderCss.logoText}`}>EXCEED</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} className="me-4" to="/">홈</Nav.Link>
-            <Nav.Link as={Link} className="me-4" to="/info">소개</Nav.Link>
-            <Nav.Link as={Link} className="me-4" to="/blog">블로그</Nav.Link>
+            <Nav.Link 
+              as={Link} 
+              className={`me-4 ${isActive('/')}`} 
+              to="/"
+              data-text="홈"
+            >홈</Nav.Link>
+            <Nav.Link 
+              as={Link} 
+              className={`me-4 ${isActive('/info')}`} 
+              to="/info"
+              data-text="소개"
+            >소개</Nav.Link>
+            <Nav.Link 
+              as={Link} 
+              className={`me-4 ${isActive('/blog')}`} 
+              to="/blog"
+              data-text="블로그"
+            >블로그</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
